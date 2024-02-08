bb45666d459e61c967735e9cd78377882425361c
Miguel Salazar <yobebe@hotmail.com.ar>
2024-02-07 21:25:05 -0300

    creando el repo de MG-lo-quiero-3d-react.js

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+//import bootstrap, { Navbar } from "react-bootstrap"; 
+import NavBar from "./components/navbar/NavBar";
+
+const App = () => {
+  return (
+    <>
+      <NavBar/>
+    </>
+  )
+}
+
+export default App
